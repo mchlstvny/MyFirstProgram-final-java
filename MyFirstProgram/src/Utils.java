a6cccae0173c90130427806d94a2b1b55dc9d3e5
Michele <micheledatii@gmail.com>
2025-02-28 08:28:15 +0800

    final version

--- MyFirstProgram/src/Utils.java
+++ MyFirstProgram/src/Utils.java
@@ -1,4 +1,4 @@
-public class Utils {
+public class Utils {        // Thread.sleep documentation from https://www.quora.com/How-do-you-print-something-after-waiting-10-seconds-in-Java
     public static void sleep(int milliseconds) {
         try {
             Thread.sleep(milliseconds);
